694f6acdb12de083ebfd07519583d2976f417805
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-25 15:03:16 +0200

    Added `AbstractPeriod`

---
+++ src/AbstractPeriod.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace RebelCode\Time;
+
+/**
+ * Abstract common functionality for periods of time.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractPeriod
+{
+    /**
+     * Retrieves the start time of this period.
+     *
+     * @since [*next-version*]
+     *
+     * @return TimeInterface
+     */
+    abstract protected function _getStart();
+
+    /**
+     * Retrieves the end time of this period.
+     *
+     * @since [*next-version*]
+     *
+     * @return TimeInterface
+     */
+    abstract protected function _getEnd();
+
+    /**
+     * Retrieves the duration of this period.
+     *
+     * @since [*next-version*]
+     *
+     * @return int An integer number of seconds. May be negative if the start time is larger than the end time.
+     */
+    protected function _getDuration()
+    {
+        return $this->_getEnd()->getTimestamp() - $this->_getStart()->getTimestamp();
+    }
+}
