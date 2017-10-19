001c384858d93e190882d02fc8dc3b2bac174fd2
Miguel Muscat <miguelmuscat93@gmail.com>
2017-10-19 18:20:08 +0200

    Added `DurationAwareTrait`

---
+++ src/DurationAwareTrait.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace RebelCode\Time;
+
+use Dhii\Util\String\StringableInterface as Stringable;
+use Exception as RootException;
+use InvalidArgumentException;
+
+/**
+ * Common functionality for objects that are aware of a duration.
+ *
+ * @since [*next-version*]
+ */
+trait DurationAwareTrait
+{
+    /**
+     * An integer number of seconds.
+     *
+     * @since [*next-version*]
+     *
+     * @var int
+     */
+    protected $duration;
+
+    /**
+     * Retrieves the timestamp.
+     *
+     * @since [*next-version*]
+     *
+     * @return int An integer number of seconds since unix epoch.
+     */
+    protected function _getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Sets the timestamp.
+     *
+     * @since [*next-version*]
+     *
+     * @param int|string|Stringable $duration The number of seconds since unix epoch. Negative numbers are allowed.
+     */
+    protected function _setDuration($duration)
+    {
+        try {
+            $this->duration = $this->_sanitizeTimestamp($duration);
+        } catch (InvalidArgumentException $exception) {
+            throw $this->_createInvalidArgumentException(
+                $this->__('Argument is not a valid duration'),
+                null,
+                $exception,
+                $duration
+            );
+        }
+    }
+
+    /**
+     * Sanitizes a timestamp to an integer number.
+     *
+     * @since [*next-version*]
+     *
+     * @param int|string|Stringable|null $timestamp The timestamp.
+     *
+     * @return int The sanitized timestamp.
+     */
+    abstract protected function _sanitizeTimestamp($timestamp);
+
+    /**
+     * Creates a new invalid argument exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable|null $message  The error message, if any.
+     * @param int|null               $code     The error code, if any.
+     * @param RootException|null     $previous The inner exception for chaining, if any.
+     * @param mixed|null             $argument The invalid argument, if any.
+     *
+     * @return InvalidArgumentException The new exception.
+     */
+    abstract protected function _createInvalidArgumentException(
+        $message = null,
+        $code = null,
+        RootException $previous = null,
+        $argument = null
+    );
+
+    /**
+     * Translates a string, and replaces placeholders.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $string  The format string to translate.
+     * @param array  $args    Placeholder values to replace in the string.
+     * @param mixed  $context The context for translation.
+     *
+     * @return string The translated string.
+     */
+    abstract protected function __($string, $args = [], $context = null);
+}
