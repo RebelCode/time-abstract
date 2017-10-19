ca9089a66f1898da3b0676bef3794b4011550259
Miguel Muscat <miguelmuscat93@gmail.com>
2017-10-19 18:15:06 +0200

    Removed unneccessary abstract methods

--- src/StartAwareTrait.php
+++ src/StartAwareTrait.php
@@ -3,8 +3,6 @@
 namespace RebelCode\Time;
 
 use Dhii\Util\String\StringableInterface as Stringable;
-use Exception as RootException;
-use InvalidArgumentException;
 
 /**
  * Common functionality for objects that are aware of a start timestamp.
@@ -57,36 +55,4 @@ trait StartAwareTrait
      * @return int The sanitized timestamp.
      */
     abstract protected function _sanitizeTimestamp($timestamp);
-
-    /**
-     * Creates a new invalid argument exception.
-     *
-     * @since [*next-version*]
-     *
-     * @param string|Stringable|null $message  The error message, if any.
-     * @param int|null               $code     The error code, if any.
-     * @param RootException|null     $previous The inner exception for chaining, if any.
-     * @param mixed|null             $argument The invalid argument, if any.
-     *
-     * @return InvalidArgumentException The new exception.
-     */
-    abstract protected function _createInvalidArgumentException(
-        $message = null,
-        $code = null,
-        RootException $previous = null,
-        $argument = null
-    );
-
-    /**
-     * Translates a string, and replaces placeholders.
-     *
-     * @since [*next-version*]
-     *
-     * @param string $string  The format string to translate.
-     * @param array  $args    Placeholder values to replace in the string.
-     * @param mixed  $context The context for translation.
-     *
-     * @return string The translated string.
-     */
-    abstract protected function __($string, $args = [], $context = null);
 }
