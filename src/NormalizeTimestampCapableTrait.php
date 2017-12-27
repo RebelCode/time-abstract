bb48582f290e8d547b35e13cbc7acff9d36cfaa8
Miguel Muscat <miguelmuscat93@gmail.com>
2017-12-27 14:23:43 +0100

    Refactored `SanitizeTimestampCapableTrait`
    - Now called `NormalizeTimestampCapableTrait`
    - No longer accepts null values
    - Depends on `_normalizeInt`

---
+++ src/NormalizeTimestampCapableTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace RebelCode\Time;
+
+use Dhii\Util\String\StringableInterface as Stringable;
+use InvalidArgumentException;
+
+/**
+ * Common functionality for objects that can normalize timestamps.
+ *
+ * @since [*next-version*]
+ */
+trait NormalizeTimestampCapableTrait
+{
+    /**
+     * Normalizes a timestamp to an integer number.
+     *
+     * @since [*next-version*]
+     *
+     * @param int|float|string|Stringable $timestamp The timestamp.
+     *
+     * @throws InvalidArgumentException If value cannot be normalized.
+     *
+     * @return int The normalized timestamp.
+     */
+    protected function _normalizeTimestamp($timestamp)
+    {
+        return $this->_normalizeInt($timestamp);
+    }
+
+    /**
+     * Normalizes a value into an integer.
+     *
+     * The value must be a whole number, or a string representing such a number,
+     * or an object representing such a string.
+     *
+     * @since [*next-version*]
+     *
+     * @param int|float|string|Stringable $value The value to normalize.
+     *
+     * @throws InvalidArgumentException If value cannot be normalized.
+     *
+     * @return int The normalized value.
+     */
+    abstract protected function _normalizeInt($value);
+}
